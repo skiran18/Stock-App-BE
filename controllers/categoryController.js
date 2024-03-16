39485187384a734edc081b156417da2bdd406a08
Sai Kiran <skiran.mekala@gmail.com>
2024-03-16 19:32:48 +0000

    category addition middleware

--- controllers/categoryController.js
+++ controllers/categoryController.js
@@ -1,21 +1,62 @@
-const { db_connection } = require('../utils/databaseCon');
+const { db_connection } = require("../utils/databaseCon");
 
 const getCategories = async (req, res) => {
-  let categories =db_connection.collection("category").find()
-  categories = await categories.toArray()
+  let categories = db_connection.collection("category").find();
+  categories = await categories.toArray();
   console.log(categories);
-  res.send(JSON.stringify({storeCategories: categories}))
+  res.send(JSON.stringify({ storeCategories: categories }));
 };
 
-
 const getStoreCategories = async (req, res) => {
-    db_connection
-      .collection("category")
-      .findOne({ storeCode: req.params.storecode })
-      .then((obj) => {
-        console.log(obj);
-        res.send(JSON.stringify({categoryStoreWise: obj }));
-      });
-  };
+  db_connection
+    .collection("category")
+    .findOne({ storeCode: req.params.storecode })
+    .then((obj) => {
+      console.log(obj);
+      res.send(JSON.stringify({ categoryStoreWise: obj }));
+    });
+};
 
-module.exports = {getCategories, getStoreCategories};
+const addCategory = async (req, res) => {
+  // req.body.newCategory = <new category string>
+  // req.body.storecode = <store code>
+  db_connection
+    .collection("category")
+    .findOne({ storeCode: req.body.storecode })
+    .then((obj) => {
+      let oldCategories = obj.categories;
+      console.log(oldCategories);
+      const filter = { storeCode: req.body.storecode };
+      const updateCategory = {
+        $push: {
+          categories: req.body.newCategory,
+        },
+      };
+      db_connection
+        .collection("category")
+        .updateOne(filter, updateCategory)
+        .then((result) => {
+            db_connection.collection("stock")
+            .findOne({ storeCode: req.body.storecode })
+            .then((storeobj) => {
+              console.log(storeobj);
+              let newcat = req.body.newCategory
+            let inner_obj = {...storeobj.stock.categories,[newcat]:[]}
+            const updateStock ={
+                $set: { stock:
+                    {categories: inner_obj}
+            }}
+          db_connection
+            .collection("stock")
+            .findOneAndUpdate(filter,updateStock)
+            .then((res2)=>{
+                console.log(res2);
+                res.send(JSON.stringify({ updatedCategory: res2 }));
+            })
+            });
+            
+        });
+    });
+};
+
+module.exports = { getCategories, getStoreCategories, addCategory };
--- routes/categoryRoute.js
+++ routes/categoryRoute.js
@@ -2,7 +2,9 @@ const express = require("express");
 const categoryController = require("../controllers/categoryController");
 const router = express.Router();
 
-router.route("/").get(categoryController.getCategories);
+router.route("/").get(categoryController.getCategories).post(categoryController.addCategory);
 router.route("/:storecode").get(categoryController.getStoreCategories);
+
+
 
 module.exports = router;
