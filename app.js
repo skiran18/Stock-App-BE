e9cc607c63ad0e0b3e25398d36caf84112bafcaf
Sai Kiran <skiran.mekala@gmail.com>
2024-03-21 20:45:02 +0000

    CORS

--- app.js
+++ app.js
@@ -9,6 +9,14 @@ const loginRouter= require('./routes/loginRoute')
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 
+var corsOptions = function(req, res, next){ 
+    res.header('Access-Control-Allow-Origin', '*'); 
+    res.header('Access-Control-Allow-Methods', 'GET,PUT,POST,DELETE,OPTIONS');
+    res.header('Access-Control-Allow-Headers', 'Content-Type');
+     next();
+}
+app.use(corsOptions);
+
 app.use("/login",loginRouter);
 app.use("/home",homeRouter);
 app.use("/category",categoryRouter);
